70e4dbd04eef718da534f4b561dcacc9ae1aa8d1
Guilherme Ferreira de Souza <141196867+GuiferrSouza@users.noreply.github.com>
2025-10-16 23:03:01 -0300

    + confid. projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,4 +1,5 @@
-import _1img from "../resources/1.jpeg";
+// import _1img from "../resources/1.jpeg";
+import _1img from "../confidential-projects/quote-generator/images/1.jpg";
 import _2img from "../resources/2.jpeg";
 import _3img from "../resources/3.jpeg";
 
@@ -11,7 +12,7 @@ const projectsData = [
         details: "Gerenciador de painéis baseado em catálogo de peças com exportação para Word. A solução busca automaticamente peças no banco de dados do usuário e organiza os dados em uma planilha Excel, que também contém informações de montagens, como painéis e kits de peças. Por meio de botões personalizados na faixa de opções do Excel, o usuário pode adicionar peças em grupos com dados expansíveis e contabilizáveis, e exporta-los para um documento Word customizado pelo próprio usuário, utilizando placeholders específicos.",
         techs: ["VBA", "Interop.Word", "Interop.Excel", "RibbonX Editor"],
         company: "EPLAN Brasil",
-        obs: "Código protegido por questões de confidencialidade.",
+        obs: "🔒 Código protegido por questões de confidencialidade.",
         gallery: [_1img, _2img, _3img]
     }
 ];
