46331c595cfc99671ebadf223e2286243fcb1011
Guilherme Ferreira de Souza <141196867+GuiferrSouza@users.noreply.github.com>
2025-10-17 17:01:59 -0300

    Changes in Projects Session

--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -1,31 +1,28 @@
-import { useState, forwardRef } from "react";
+import { forwardRef } from 'react';
+import Microlink from '@microlink/react'
 import data from "../../data/projectsData";
-import ProjectModal from "../ProjectModal/ProjectModal";
 import "./ProjectsSection.css";
 
-const ProjectsSection = forwardRef((_, ref) => {
-  const [selectedProject, setSelectedProject] = useState(null);
+<Microlink url='https://www.youtube.com/watch?v=9P6rdqiybaw' />
 
+const ProjectsSection = forwardRef((_, ref) => {
   return (
     <section id="projects" className="projects" ref={ref}>
       <h2 className="fade">Projetos</h2>
-      <div className="projects-grid">
+      <div className="project-list">
         {data.map((proj) => (
-          <div key={proj.id} className="project-item fade" onClick={() => setSelectedProject(proj)}>
-            <div className="image-wrapper">
-              <img src={proj.image} alt={proj.title} />
-              <div className={`status-badge ${proj.status === "Completo" ? "complete" : "in-progress"}`}>
-                {proj.status}
-              </div>
+          <div key={proj.id} className="project-item fade">
+            <div className="project-item-content">
+              <h4 className="project-title">{proj.title}</h4>
+              <p className="project-details">
+                {proj.private && <p className="project-private">Código protegido por questões de confidencialidade.</p>}
+                <span className="project-status">{proj.status}</span>
+              </p>
             </div>
-            <h5 className="project-title">{proj.title}</h5>
+            <Microlink url="https://www.youtube.com/watch?v=9P6rdqiybaw" size="small"/>
           </div>
         ))}
       </div>
-
-      {selectedProject && (
-        <ProjectModal visible={!!selectedProject} project={selectedProject} handleClose={() => setSelectedProject(null)} />
-      )}
     </section>
   );
 });
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,20 +1,34 @@
-// import _1img from "../resources/1.jpeg";
-import _1img from "../confidential-projects/quote-generator/images/1.jpg";
-import _2img from "../resources/2.jpeg";
-import _3img from "../resources/3.jpeg";
-
 const projectsData = [
     {
         id: 1,
-        title: "Gerador de Orçamentos",
+        title: "Sincronizador de Bancos de Dados",
+        url: "",
         status: "Completo",
-        image: _1img,
-        details: "Gerenciador de painéis baseado em catálogo de peças com exportação para Word. A solução busca automaticamente peças no banco de dados do usuário e organiza os dados em uma planilha Excel, que também contém informações de montagens, como painéis e kits de peças. Por meio de botões personalizados na faixa de opções do Excel, o usuário pode adicionar peças em grupos com dados expansíveis e contabilizáveis, e exporta-los para um documento Word customizado pelo próprio usuário, utilizando placeholders específicos.",
-        techs: ["VBA", "Interop.Word", "Interop.Excel", "RibbonX Editor"],
-        company: "EPLAN Brasil",
-        obs: "🔒 Código protegido por questões de confidencialidade.",
-        gallery: [_1img, _2img, _3img]
+        private: true
+    },
+    {
+        id: 2,
+        title: "Sincronizador de Bancos de Dados",
+        url: "",
+        status: "Completo"
+    },
+    {
+        id: 3,
+        title: "Sincronizador de Bancos de Dados",
+        url: "",
+        status: "Completo"
     }
+    // {
+    //     id: 1,
+    //     title: "Gerador de Orçamentos",
+    //     status: "Completo",
+    //     image: _1img,
+    //     details: "Gerenciador de painéis baseado em catálogo de peças com exportação para Word. A solução busca automaticamente peças no banco de dados do usuário e organiza os dados em uma planilha Excel, que também contém informações de montagens, como painéis e kits de peças. Por meio de botões personalizados na faixa de opções do Excel, o usuário pode adicionar peças em grupos com dados expansíveis e contabilizáveis, e exporta-los para um documento Word customizado pelo próprio usuário, utilizando placeholders específicos.",
+    //     techs: ["VBA", "Interop.Word", "Interop.Excel", "RibbonX Editor"],
+    //     company: "EPLAN Brasil",
+    //     obs: "🔒 Código protegido por questões de confidencialidade.",
+    //     gallery: [_1img, _2img, _3img]
+    // }
 ];
 
 export default projectsData;
